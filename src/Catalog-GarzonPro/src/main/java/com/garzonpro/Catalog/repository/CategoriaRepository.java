8ce81294d41df99292d70b99bb4f5b8cae56a071
Benjja006 <sepulvedabenjamin464@gmail.com>
2026-06-16 17:21:25 -0400

    Limpiar rama docs y agregar nuevo PDF

--- src/Catalog-GarzonPro/src/main/java/com/garzonpro/Catalog/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.garzonpro.Catalog.repository;
-
-import com.garzonpro.Catalog.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
